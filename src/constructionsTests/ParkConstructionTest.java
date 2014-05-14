4df01e3ec375735407dd484024ded3af77330df7
Jose Moreira <ei12002@fe.up.pt>
2014-05-14 20:45:54 +0100

    Some more tests added

---
+++ src/constructionsTests/ParkConstructionTest.java
@@ -0,0 +1,38 @@
+package constructionsTests;
+
+import static org.junit.Assert.*;
+import mainPackage.State;
+import mainPackage.Tile;
+import mainPackage.constructions.AirportConstruction;
+import mainPackage.constructions.FactoryConstruction;
+import mainPackage.constructions.ParkConstruction;
+import mainPackage.constructions.Construction;
+
+import org.junit.Test;
+
+public class ParkConstructionTest {
+
+	@Test
+	public void testAdjacenciesConstraintPlusInclinationGivingZeroAffinity() {
+		ParkConstruction park=new ParkConstruction();
+		AirportConstruction airport=new AirportConstruction();
+		FactoryConstruction factory=new FactoryConstruction();
+		
+		Tile park_tile=new Tile();
+		Tile airport_tile=new Tile();
+		Tile factory_tile=new Tile();
+		park_tile.setMaxInclination(0.6);
+		
+		park_tile.addAdjacentTile(airport_tile);
+		park_tile.addAdjacentTile(factory_tile);
+		
+		
+		State s=new State(new Construction[]{park,airport,factory},new Tile[]{park_tile,airport_tile,factory_tile});
+		
+		double aff=park.affinityToTileInState(park_tile, s);
+		
+		assertEquals(0.0,aff,0.00001);
+		
+	}
+
+}
---
+++ src/constructionsTests/PrisonConstructionTest.java
@@ -0,0 +1,63 @@
+package constructionsTests;
+
+import static org.junit.Assert.*;
+import mainPackage.State;
+import mainPackage.Tile;
+import mainPackage.constructions.AirportConstruction;
+import mainPackage.constructions.Construction;
+import mainPackage.constructions.PrisonConstruction;
+
+import org.junit.Test;
+
+public class PrisonConstructionTest {
+
+	@Test
+	public void testPenaltyAirportNear() {
+		PrisonConstruction pri=new PrisonConstruction();
+		AirportConstruction air=new AirportConstruction();
+		
+		Tile prisonTile=new Tile();
+		Tile airportTile=new Tile();
+		prisonTile.addAdjacentTile(airportTile);
+		
+		State s=new State(new Construction[]{pri,air},new Tile[]{prisonTile,airportTile} );
+		
+		double aff=pri.affinityToTileInState(prisonTile, s);
+		
+		assertEquals(0.7, aff,0.00001);
+	}
+	
+	
+	@Test
+	public void testPenaltyAirportNearAndHighPrice() {
+		PrisonConstruction pri=new PrisonConstruction();
+		AirportConstruction air=new AirportConstruction();
+		
+		Tile prisonTile=new Tile();
+		Tile airportTile=new Tile();
+		prisonTile.addAdjacentTile(airportTile);
+		prisonTile.setPricePerAreaUnit(100);
+		
+		State s=new State(new Construction[]{pri,air},new Tile[]{prisonTile,airportTile} );
+		
+		double aff=pri.affinityToTileInState(prisonTile, s);
+		
+		assertEquals(0.65, aff,0.00001);
+	}
+	
+	@Test
+	public void noPenalty() {
+		PrisonConstruction pri=new PrisonConstruction();
+		AirportConstruction air=new AirportConstruction();
+		
+		Tile prisonTile=new Tile();
+		Tile airportTile=new Tile();
+		
+		State s=new State(new Construction[]{pri,air},new Tile[]{prisonTile,airportTile} );
+		
+		double aff=pri.affinityToTileInState(prisonTile, s);
+		
+		assertEquals(1.0, aff,0.00001);
+	}
+
+}
